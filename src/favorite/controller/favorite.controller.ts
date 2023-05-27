3cd96b6977ccd16c201ac6a3a44bf7c4bb07ec01
onlyhappiness <onlyhappiness96@gmail.com>
2023-05-27 23:05:28 +0900

    Feat: Favorite 작업 중..

--- src/favorite/controller/favorite.controller.ts
+++ src/favorite/controller/favorite.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { FavoriteService } from '@favorite/service/favorite.service';
+import { Controller, Delete, Get, Post } from '@nestjs/common';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Favorite')
 @Controller('favorite')
-export class FavoriteController {}
+export class FavoriteController {
+  constructor(private readonly favoriteService: FavoriteService) {}
+
+  @ApiOperation({ summary: '찜한 커뮤니티 보기' })
+  @Get()
+  async findFavorite() {
+    return '찜한 커뮤니티 보기';
+  }
+
+  @ApiOperation({ summary: '찜하기' })
+  @Post()
+  async createFavorite() {
+    return '찜하기';
+  }
+
+  @ApiOperation({ summary: '찜한 커뮤니티 삭제' })
+  @Delete()
+  async deleteFavorite() {
+    return '찜한 커뮤니티 삭제';
+  }
+}
