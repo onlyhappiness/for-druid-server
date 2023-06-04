40ed5dde14417aed917aa07aa1e1ef7f1e07903c
onlyhappiness <onlyhappiness96@gmail.com>
2023-06-04 22:23:09 +0900

    :sparkles: Feat: 찜 삭제 기능 추가

--- src/favorite/controller/favorite.controller.ts
+++ src/favorite/controller/favorite.controller.ts
@@ -2,7 +2,15 @@ import { JwtAuthGuard } from '@auth/jwt/jwt.guard';
 import { CurrentUser } from '@common/decorators/user.decorator';
 import { CreateFavoriteDTO } from '@favorite/dto/create.favorite.dto';
 import { FavoriteService } from '@favorite/service/favorite.service';
-import { Body, Controller, Delete, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  UseGuards,
+} from '@nestjs/common';
 import { ApiBearerAuth, ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Users } from '@user/model/user.entity';
 
@@ -31,9 +39,14 @@ export class FavoriteController {
     return this.favoriteService.createFavorite(CurrentUser, body);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @ApiOperation({ summary: '찜한 커뮤니티 삭제' })
-  @Delete()
-  async deleteFavorite() {
-    return this.favoriteService.deleteFavorite();
+  @Delete('/:favoriteId')
+  async deleteFavorite(
+    @Param('favoriteId') favoriteId: number,
+    @CurrentUser() currentUser: Users,
+  ) {
+    return this.favoriteService.deleteFavorite(favoriteId, currentUser);
   }
 }
--- src/favorite/service/favorite.service.ts
+++ src/favorite/service/favorite.service.ts
@@ -2,7 +2,7 @@ import { AuthService } from '@auth/service/auth.service';
 import { CommunityService } from '@community/service/community.service';
 import { CreateFavoriteDTO } from '@favorite/dto/create.favorite.dto';
 import { Favorite } from '@favorite/model/favorite.entity';
-import { Injectable } from '@nestjs/common';
+import { HttpException, Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Users } from '@user/model/user.entity';
 import { plainToInstance } from 'class-transformer';
@@ -16,6 +16,22 @@ export class FavoriteService {
     private readonly authService: AuthService,
     private readonly communityService: CommunityService,
   ) {}
+
+  //** 찜 유저 찾기 */
+  async findFavoriteByUser(favoriteId, userId) {
+    console.log('favoriteId: ', favoriteId);
+    console.log('userId: ', userId);
+
+    const favorite = await this.FavoriteRepository.findOne({
+      relations: ['Users'],
+      where: { id: favoriteId, Users: { id: userId } },
+    });
+
+    if (!favorite) {
+      throw new HttpException('존재하지 않는 게시글입니다.', 400);
+    }
+    return favorite;
+  }
 
   //** 찜 아이디로 찜 찾기 */
   async findFavoriteById(favoriteId) {
@@ -46,7 +62,11 @@ export class FavoriteService {
   }
 
   //** 찜 삭제 */
-  async deleteFavorite() {
-    return '찜 삭제';
+  async deleteFavorite(favoriteId, currentUser) {
+    const { id: userId } = currentUser;
+    await this.findFavoriteByUser(favoriteId, userId);
+    await this.FavoriteRepository.delete({ id: favoriteId });
+
+    return true;
   }
 }
