2c2279ca595c4776202aa1dd4086f8504ec2657c
onlyhappiness <onlyhappiness96@gmail.com>
2023-07-11 22:21:03 +0900

    :sparkles: Feat: 이미지 업로드 작업중

--- src/s3/controller/s3.controller.ts
+++ src/s3/controller/s3.controller.ts
@@ -1,9 +1,22 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 import { S3Service } from '../service/s3.service';
+import { FileInterceptor } from '@nestjs/platform-express';
 
 @ApiTags('Upload')
 @Controller('upload')
 export class S3Controller {
   constructor(private readonly S3Service: S3Service) {}
+
+  @Post()
+  @UseInterceptors(FileInterceptor('file'))
+  async uploadFile(@UploadedFile() image) {
+    console.log('image: ', image);
+    return await this.S3Service.uploadFile(image);
+  }
 }
